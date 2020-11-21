141e2113bd9e3236967068973058d4b7600fcb8f
César Ravelo Martínez <crmprograming@gmail.com>
2020-11-21 11:58:34 +0000

    Inicializados ContactoController y ContactoView

---
+++ src/main/java/dad/javafx/micv/contacto/ContactoController.java
@@ -0,0 +1,29 @@
+package dad.javafx.micv.contacto;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class ContactoController implements Initializable {
+	
+	@FXML
+	private VBox view;
+	
+	public ContactoController() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ContactoView.fxml"));
+		loader.setController(this);
+		loader.load();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+
+}
