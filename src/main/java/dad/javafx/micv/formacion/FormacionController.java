0a94777782ab94248c205ca38d95ded1e1ee17ee
César Ravelo Martínez <crmprograming@gmail.com>
2020-11-22 09:51:31 +0000

    Implementada la vista de Formación

--- src/main/java/dad/javafx/micv/formacion/FormacionController.java
+++ src/main/java/dad/javafx/micv/formacion/FormacionController.java
@@ -8,21 +8,33 @@ import dad.javafx.micv.model.Titulo;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 public class FormacionController implements Initializable {
 
 	// model
 
-	private ObjectProperty<Titulo> titulo = new SimpleObjectProperty<>();	
+	private ObjectProperty<Titulo> titulo = new SimpleObjectProperty<>();
 
 	// view
 
 	@FXML
 	private BorderPane view;
+
+	@FXML
+	private TableView<?> tvFormacion;
+
+	@FXML
+	private Button btAnadir;
+
+	@FXML
+	private Button btEliminar;
 
 	public FormacionController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FormacionView.fxml"));
@@ -32,23 +44,33 @@ public class FormacionController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		this.titulo.addListener((o, ov, nv) -> onPersonalChanged(o, ov, nv));
-		
+
+		this.titulo.addListener((o, ov, nv) -> onTituloChanged(o, ov, nv));
+
 	}
 
-	private void onPersonalChanged(ObservableValue<? extends Titulo> o, Titulo ov, Titulo nv) {
-		
+	private void onTituloChanged(ObservableValue<? extends Titulo> o, Titulo ov, Titulo nv) {
+
 		if (ov != null) {
-			
+
 		}
 
 		if (nv != null) {
-			
+
 		}
-		
+
 	}
-	
+
+	@FXML
+	void onClickAnadir(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onClickEliminar(ActionEvent event) {
+
+	}
+
 	public BorderPane getView() {
 		return view;
 	}
