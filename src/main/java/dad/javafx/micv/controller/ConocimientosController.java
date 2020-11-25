0b9684314bbc7de1e93dd60e8ce9424457235898
César Ravelo Martínez <crmprograming@gmail.com>
2020-11-25 16:33:12 +0000

    Implementada la creación de idioma

--- src/main/java/dad/javafx/micv/controller/ConocimientosController.java
+++ src/main/java/dad/javafx/micv/controller/ConocimientosController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 
 import dad.javafx.micv.App;
 import dad.javafx.micv.model.conocimiento.Conocimiento;
+import dad.javafx.micv.model.conocimiento.Idioma;
 import dad.javafx.micv.model.conocimiento.Nivel;
 import dad.javafx.micv.utils.ResultadosDialogoConocimiento;
 import javafx.application.Platform;
@@ -40,6 +41,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
@@ -184,7 +186,93 @@ public class ConocimientosController implements Initializable {
 	
 	@FXML
 	void onClickAnadirIdioma(ActionEvent event) {
+Dialog<ResultadosDialogoConocimiento> dialog = new Dialog<>();
 		
+		dialog.setTitle("Nuevo conocimiento");
+		
+		Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image(this.getClass().getResource("/images/cv64x64.png").toString()));
+		stage.setMinWidth(550);
+		stage.setMinHeight(300);
+		
+		ButtonType btCrear = new ButtonType("Crear", ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().addAll(btCrear, ButtonType.CANCEL);
+		
+		GridPane grid = new GridPane();
+		grid.setHgap(5);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(20, 10, 10, 10));
+		
+		TextField tfDenominacion = new TextField();
+		ComboBox<Nivel> cbNivel = new ComboBox<>();
+		Button btLimpiarCombo = new Button("X");
+		TextField tfCertificacion = new TextField();
+		HBox hBox = new HBox();
+				
+		hBox.getChildren().addAll(cbNivel, btLimpiarCombo);
+		hBox.setSpacing(5);
+		
+		tfCertificacion.setPrefWidth(160);
+		
+		cbNivel.getItems().addAll(Nivel.values());
+		
+		Node nodeBtAnadir = dialog.getDialogPane().lookupButton(btCrear);
+		nodeBtAnadir.setDisable(true);
+		
+		nodeBtAnadir.disableProperty().bind(
+				tfDenominacion.textProperty().isEmpty().or(
+				cbNivel.valueProperty().isNull()).or(
+				tfCertificacion.textProperty().isEmpty()));
+		
+		btLimpiarCombo.setOnAction(e -> {
+			cbNivel.setValue(null);
+		});
+		
+		grid.add(new Label("Denominación"), 0, 0);
+		grid.add(tfDenominacion, 1, 0);
+		grid.add(new Label("Nivel"), 0, 1);
+		grid.add(hBox, 1, 1);
+		grid.add(new Label("Certificación"), 0, 2);
+		
+		GridPane.setColumnSpan(tfDenominacion, 2);
+		grid.add(tfCertificacion, 1, 2);
+		
+		ColumnConstraints[] cols = {
+				new ColumnConstraints(),
+				new ColumnConstraints(),
+				new ColumnConstraints()
+		};
+		
+		cols[0].setHalignment(HPos.RIGHT);
+		cols[2].setHgrow(Priority.ALWAYS);
+		cols[2].setFillWidth(true);
+		cols[2].setHalignment(HPos.LEFT);
+		
+		grid.getColumnConstraints().setAll(cols);
+		
+		dialog.getDialogPane().setContent(grid);
+		
+		Platform.runLater(() -> tfDenominacion.requestFocus());
+		
+		dialog.setResultConverter(dialogButton -> {
+			if (dialogButton == btCrear) {
+				return new ResultadosDialogoConocimiento(
+						tfDenominacion.getText(),
+						cbNivel.getValue(),
+						tfCertificacion.getText());
+			}
+			return null;
+		});
+		
+		Optional<ResultadosDialogoConocimiento> result = dialog.showAndWait();
+		
+		if (result.isPresent()) {
+			Idioma idioma = new Idioma();
+			idioma.setDenominacion(result.get().getDenominacion());
+			idioma.setNivel(result.get().getNivel());
+			idioma.setCertificacion(result.get().getCertificacion());
+			conocimientos.get().add(idioma);
+		}	
 	}
 	
 	@FXML
